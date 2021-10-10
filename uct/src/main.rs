fa9a7324c018102b1bd8acba0f1d9363e33dc348
Stefan Hagen <stefan@hagen.link>
2021-10-10 17:42:09 +0200

    Added uniform cycle time example

---
+++ uct/src/main.rs
@@ -0,0 +1,34 @@
+extern crate rand;
+
+use rand::distributions::{Distribution, Uniform};
+use rand::thread_rng;
+
+const TOTAL_TRIALS: i32 = 100_000; // too low for trials but good 'nuff for development
+const TOTAL_TICKETS: i32 = 12 * 20 * 1; // months x work days x actor(s)
+const PERCENT_DOMAIN: i32 = 100;
+
+fn main() {
+    let mut rng = thread_rng();
+    let time_to_completion = Uniform::from(0..3);
+
+    let working_days_of_year = 12 * 20;
+    let mut successes = 0;
+
+    for _ in 0..TOTAL_TRIALS {
+        let mut current_duration = 0;
+
+        for _ in 0..TOTAL_TICKETS {
+            current_duration += time_to_completion.sample(&mut rng);
+        }
+
+        if current_duration <= working_days_of_year {
+            successes += 1
+        }
+    }
+
+    let prob_perc: f64 = f64::from(successes) / f64::from(TOTAL_TRIALS) * f64::from(PERCENT_DOMAIN);
+
+    println!("Total simulations: {}", TOTAL_TRIALS);
+    println!("Successes: {}", successes);
+    println!("Probability of succeeding: {:.2}%", prob_perc);
+}
